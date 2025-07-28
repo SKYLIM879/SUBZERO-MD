de3898ca231c95acb8b42064ea7e618c5fd07e54
SKYLIM879 <skylimits1887@gmail.com>
2025-07-28 19:42:33 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO~516c27e23641db763f3c1847383075ac623e185c", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "27655693708", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
